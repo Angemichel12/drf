551c94feb0410c0a3d19c6c527d0c42f5c548443
Angemichel12 <angemicheluwizeye@gmail.com>
2025-01-02 17:54:07 +0200

    updateAPIView and Destroy ApiView

--- backend/products/views.py
+++ backend/products/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
@@ -14,9 +14,30 @@ class ProductCreateAPIView(generics.CreateAPIView):
             print(content)
         serializer.save(content=content)
 
-product_create_view = ProductCreateAPIView.as_view()
+product_list_create_view = ProductListCreateAPIView.as_view()
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+product_update_apiveiw = ProductUpdateAPIView.as_view()
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
+    
+product_destroy_apiview = ProductDestroyAPIView.as_view()
